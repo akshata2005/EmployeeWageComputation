0fdfd50598609447be1beaa7f35d8e1a49e30d1b
akshata2005 <akshatasawant1805@gmail.com>
2022-03-09 15:36:21 +0530

    [Akshata] Added Employee Attendance Check Program

---
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    public class Program
+    {
+       public static void Main(string[] args)
+        {
+            //printing message on console
+            Console.WriteLine("Welcome To Employee Wage Computation Program");
+            CalculateEmpWage.WageCompute();    // calling static method using class name.
+            Console.ReadLine();
+        }
+           
+    }
+}
